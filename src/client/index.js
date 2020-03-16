4c72bfc63b8614968b24b5cf6db5312077635fc6
Angelo <angelo@rcstart.it>
2020-03-16 16:20:19 +0100

    various implementations

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,11 @@
  * @module client
  */
 import * as crypto from "../crypto"
+import HttpRequest from "../utils/request"
+import Big from "big.js"
+import { checkNumber } from "../utils/validateHelper"
+
+const BASENUMBER = Math.pow(10, 6)
 
 /**
  * The Bitsong Network client.
@@ -17,6 +22,7 @@ export class BitSongClient {
       throw new Error("BitSong Network Server should not be null")
     }
     this.addressPrefix = "bitsong"
+    this._httpClient = new HttpRequest(server)
     // this._httpClient = new HttpRequest(server)
     // this._signingDelegate = DefaultSigningDelegate
     // this._broadcastDelegate = DefaultBroadcastDelegate
@@ -25,6 +31,18 @@ export class BitSongClient {
     // this.tokens = new TokenManagement(this)
     // this.swap = new Swap(this)
     // this.gov = new Gov(this)
+  }
+
+  /**
+   * Initialize the client with the chain's ID. Asynchronous.
+   * @return {Promise}
+   */
+  async initChain() {
+    if (!this.chainId) {
+      const data = await this._httpClient.request("get", api.nodeInfo)
+      this.chainId = data.result.node_info && data.result.node_info.network
+    }
+    return this
   }
 
   /**
@@ -132,10 +150,6 @@ export class BitSongClient {
       address
     }
   }
-  // support an old method name containing a typo
-  recoverAccountFromMneomnic(mnemonic) {
-    return this.recoverAccountFromMnemonic(mnemonic)
-  }
 
   /**
    * Recovers an account using private key.
@@ -180,4 +194,104 @@ export class BitSongClient {
     this.address = address
     return address
   }
+
+  /**
+   * Send tokens from one address to another.
+   * @param {String} fromAddress
+   * @param {String} toAddress
+   * @param {Number} amount
+   * @param {String} asset
+   * @param {String} memo optional memo
+   * @param {Number} sequence optional sequence
+   * @return {Promise} resolves with response (success or fail)
+   */
+  async send(fromAddress, toAddress, amount, asset, memo = "", sequence = null) {
+    const accCode = crypto.decodeAddress(fromAddress)
+    const toAccCode = crypto.decodeAddress(toAddress)
+
+    amount = new Big(amount)
+    amount = Number(amount.mul(BASENUMBER).toString())
+
+    checkNumber(amount, "amount")
+
+    const coin = {
+      denom: asset,
+      amount: amount,
+    }
+
+    const msg = {
+      inputs: [{
+        address: accCode,
+        coins: [coin]
+      }],
+      outputs: [{
+        address: toAccCode,
+        coins: [coin]
+      }],
+      msgType: "MsgSend"
+    }
+
+    const msgs = [{
+      type: "cosmos-sdk/MsgSend",
+      value: {
+        amount: [coin],
+        from_address: accCode,
+        to_address: toAccCode
+      }
+    }];
+
+    const signMsg = {
+      inputs: [{
+        address: fromAddress,
+        coins: [{
+          amount: amount,
+          denom: asset
+        }]
+      }],
+      outputs: [{
+        address: toAddress,
+        coins: [{
+          amount: amount,
+          denom: asset
+        }]
+      }]
+    }
+
+    const signedTx = await this._prepareTransaction(msg, signMsg, fromAddress, sequence, memo)
+    return this._broadcastDelegate(signedTx)
+  }
+
+  /**
+   * Prepare a serialized raw transaction for sending to the blockchain.
+   * @param {Object} msg the msg object
+   * @param {Object} stdSignMsg the sign doc object used to generate a signature
+   * @param {String} address
+   * @param {Number} sequence optional sequence
+   * @param {String} memo optional memo
+   * @return {Transaction} signed transaction
+   */
+  async _prepareTransaction(msg, stdSignMsg, address, sequence = null, memo = "") {
+    if ((!this.account_number || (sequence !== 0 && !sequence)) && address) {
+      const data = await this._httpClient.request("get", `${api.getAccount}/${address}`)
+      sequence = data.result.sequence
+      this.account_number = data.result.account_number
+      // if user has not used `await` in its call to setPrivateKey (old API), we should wait for the promise here
+    } else if (this._setPkPromise) {
+      await this._setPkPromise
+    }
+
+    const options = {
+      account_number: parseInt(this.account_number),
+      chain_id: this.chainId,
+      memo: memo,
+      msg,
+      sequence: parseInt(sequence),
+      source: this._source,
+      type: msg.msgType,
+    }
+
+    const tx = new Transaction(options)
+    return this._signingDelegate.call(this, tx, stdSignMsg)
+  }
+
 }
