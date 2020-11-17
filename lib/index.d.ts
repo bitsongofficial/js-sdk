d6f81d207dc41a1c720aec5b123cfbc912949279
Angelo <angelo@rcstart.it>
2020-11-17 13:08:50 +0100

    Fix Tx import

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,6 @@
 import "./declarations";
 import { BitSongClient } from "./client";
 import * as crypto from "./crypto";
-import * as tx from "./tx";
+import { Transaction } from "./tx";
 import * as utils from "./utils";
-export { BitSongClient, crypto, utils, tx };
+export { BitSongClient, crypto, utils, Transaction };
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "./declarations"
 
 import { BitSongClient } from "./client"
 import * as crypto from "./crypto"
-import * as tx from "./tx"
+import { Transaction } from "./tx"
 import * as utils from "./utils"
 
-export { BitSongClient, crypto, utils, tx }
+export { BitSongClient, crypto, utils, Transaction }
